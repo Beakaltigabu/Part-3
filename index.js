a0da94055caf5505b4766e3b18b09d784b016edb
Beakaltigabu <113409071+Beakaltigabu@users.noreply.github.com>
2025-07-10 16:20:35 +0300

    exercise 3.19/ mongoose validation added

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ morgan.token('body', (req)=>{
 const errorHandler=(error,request, response, next )=>{
   if(error.name==='CastError'){
     return response.status(400).send({error:'manlformatted it'})
+  } else if(error.name==='ValidationError'){
+    return response.status(400).json({error:error.message})
   }
   next(error)
 }
